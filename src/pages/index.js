5485e569143026c7e6fc7b5a61e75e549f1b3e49
milesmanos <milesarthurkim@gmail.com>
2022-08-02 10:56:27 -0700

    lesson 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import * as homeStyles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Hello world!</h1>
+      <section className={homeStyles.header}>
+        <h1>Hello world!</h1>
+        <Link className={homeStyles.btn} to="/projects">
+          My Portfolio
+        </Link>
+      </section>
     </Layout>
   )
 }
