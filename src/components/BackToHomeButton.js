1fe106d627538355570ea8a390e130d28b6c53b8
milesmanos <milesarthurkim@gmail.com>
2023-01-10 16:36:45 -0800

    isBroswer

--- src/components/BackToHomeButton.js
+++ src/components/BackToHomeButton.js
@@ -4,25 +4,24 @@ import Button from "../components/Button"
 import { CloseSVG } from "../icons/CloseSVG"
 
 export default function BackToHomeButton() {
-  if (document.referrer.includes(window.location.host)) {
-    return (
-      <Button isFullWidth onClick={() => navigate(-1)}>
-        <div className="text">Back</div>
+  const isBrowser = () => typeof window !== "undefined"
+  return isBrowser() &&
+    window.history.length > 1 &&
+    document.referrer.includes(window.location.host) ? (
+    <Button isFullWidth onClick={() => navigate(-1)}>
+      <div className="text">Back</div>
+      <div className="icon">
+        <CloseSVG size={16} />
+      </div>
+    </Button>
+  ) : (
+    <Link style={{ width: "100%" }} to="/">
+      <Button isFullWidth>
+        <div className="text">Home</div>
         <div className="icon">
           <CloseSVG size={16} />
         </div>
       </Button>
-    )
-  } else {
-    return (
-      <Link style={{ width: "100%" }} to="/">
-        <Button isFullWidth>
-          <div className="text">Home</div>
-          <div className="icon">
-            <CloseSVG size={16} />
-          </div>
-        </Button>
-      </Link>
-    )
-  }
+    </Link>
+  )
 }
